22868d41a9ea01757499e93de6005212bbf5a283
Utkarsh Marwaha <u6146791@anu.edu.au>
2019-06-29 17:09:08 +1000

    merge sort added

--- src/_2_Sorting/MergeSort.java
+++ src/_2_Sorting/MergeSort.java
@@ -1,99 +1,139 @@
 package _2_Sorting;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeSort {
 
-    // Merges two sub arrays of items[].
-    // First subarray is items[left..middle]
-    // Second subarray is items[middle+1..right]
-
-    private static void merge(int [] items, int left, int middle, int right) {
-
-        // Find sizes of two sub arrays to be merged
-        int sizeLeft = middle - left + 1;
-        int sizeRight = right - middle;
-
-        /* Create temp arrays */
-        int [] leftArray = new int [sizeLeft];
-        int [] rightArray = new int [sizeRight];
-
-        /*Copy data to temp arrays*/
-        System.arraycopy(items,left, leftArray, 0, sizeLeft);
-        System.arraycopy(items,middle+1, rightArray, 0, sizeRight);
-
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second sub arrays
-        int i = 0, j = 0;
-
-        // Initial index of merged sub array array
-        int k = left;
-
-        while (i < sizeLeft && j < sizeRight) {
-
-            if (leftArray[i] <= rightArray[j]) {
-                items[k] = leftArray[i];
-                i++;
-            }
-            else {
-                items[k] = rightArray[j];
-                j++;
-            }
-            k++;
-        }
-
-        /* Copy remaining elements of leftArray[] if any */
-        while (i < sizeLeft) {
-            items[k] = leftArray[i];
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of rightArray[] if any */
-        while (j < sizeRight) {
-            items[k] = rightArray[j];
-            j++;
-            k++;
-        }
+    /**
+     * Time Complexity: O(N log N)
+     * Space Complexity: O(N)
+     *
+     * Merge sort is a stable algorithm because its preserves
+     * the relative ordering of duplicate items in the resultant array
+     *
+     * @param input array to be sorted
+     */
+    private static void mergeSort(int[] input){
+        mergeSort(input, 0, input.length);
     }
 
-    // Main function that sorts items[left..right] using
-    // merge()
-    private static void sort(int [] items, int left, int right) {
+    private static void mergeSort(int[] input, int start, int end){
 
-        if (left < right) {
-            // Find the middle point
-            int middle = (left+right)/2;
-
-            // Sort first and second halves
-            sort(items, left, middle);
-            sort(items , middle+1, right);
-
-            // Merge the sorted halves
-            merge(items, left, middle, right);
+        // if we have received a 1-element array
+        if (end - start == 1){
+            return;
         }
+
+        // calculate the mid point
+        int mid = (start + end) / 2;
+
+        // sort the left partition
+        mergeSort(input, 0, mid);
+        // sort the right partition
+        mergeSort(input, mid, end);
+
+        // merge the left and right partitions
+        merge(input, start, mid, end);
+
+    }
+
+    /**
+     *
+     * @param input array whose partitions are to be merged
+     * @param start start of the left array
+     * @param mid start of the right array
+     * @param end end of the right array
+     */
+    private static void merge(int[] input, int start, int mid, int end) {
+
+        /*
+         * input[mid-1] is the biggest (last) element of the left array and
+         * input[mid] is the smallest (first) element of the right array
+         *
+         * Since we are merging two sorted arrays therefore all the elements
+         * in the left array are smaller than all the elements of the
+         * right array. We can simply put them together
+         */
+        if (input[mid-1] <= input[mid]){
+            return;
+        }
+
+        // index representing the start of the left array
+        int i = start;
+
+        // index representing the start of the right array
+        int j = mid;
+
+        // this keeps track of the current index in the temporary array
+        int tempIndex = 0;
+
+        // create a temporary array of the combined size of left and right arrays
+        int[] temp = new int[end-start];
+
+        // mid indicates the end of left array
+        // end indicates the end of the right array
+        while (i < mid && j < end){
+            // temp gets the smaller of the two elements
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        // check which array has been exhausted
+        int remS = i < mid ? i : j;
+        int remE = i < mid ? mid: end;
+
+        // copy over the remaining elements into the temporary array
+        for (int p = remS; p < remE; p++){
+            temp[tempIndex++] = input[p];
+        }
+
+        // copy over the temp array to the original array
+        System.arraycopy(temp, 0, input, start, tempIndex);
     }
 
     public static void main(String[] args) {
 
+        // Creating an object of class Scanner
         Scanner in = new Scanner(System.in);
 
-        System.out.println("How many elements are to be sorted?");
+        // Asking the user to enter the size of the array
+        System.out.println("Enter the size of the array: ");
         int size = in.nextInt();
 
-        int [] array = new int[size];
+        // declaring the array
+        int array[] = new int[size];
 
-        for (int i = 0; i < size; i++){
-            System.out.println("Enter element "+(i+1));
+        // Asking the user to enter the elements of the array
+        for (int i = 0; i<array.length; i++){
+            System.out.println("Enter element" + (i+1));
             array[i] = in.nextInt();
         }
 
-        sort(array, 0, array.length-1);
-        System.out.print("The sorted array is: ");
-        for (int num: array){
-            System.out.print(num+" ");
-        }
+        // Displaying the result
+        mergeSort(array);
+        System.out.println(Arrays.toString(array));
     }
 }
+
+
+
+/*
+Merge sort is an example of a divide and conquer algorithm. It is usually implemented recursively.
+Merge sort consists of two phases:-
+1. Splitting Phase
+    The concept of splitting is logical i.e. we do not create new arrays during this process
+    In the splitting phase,
+    a. We start with the an unsorted array.
+    b. Divide the array into 2 arrays, which are unsorted. The first array is the left array, and the
+       second array is the right array.
+    c. Split the left and the right arrays into two arrays each.
+    d. Keep splitting until all arrays have only one element each -- these arrays are sorted.
+2. Merging Phase
+   In the merging phase,
+   a. Merge every left/right pair of sibling arrays into a sorted array.
+   b. After the first merge, we'll have a bunch of 2-element sorted arrays.
+   c. Then merge those sorted arrays (left/right siblings) to end up with a bunch
+      of 4-element sorted arrays.
+   d. Repeat until you have a single sorted array.
+   e. Not-in place. Uses temporary arrays.
+ */
