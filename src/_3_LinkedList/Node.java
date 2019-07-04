1566127860eb41e6c0f5d2dc9803b080405660cd
u6146791 <u6146791@anu.edu.au>
2019-07-04 10:49:36 +1000

    singly linked list: append and search added

---
+++ src/_3_LinkedList/Node.java
@@ -0,0 +1,62 @@
+package _3_LinkedList;
+
+public class Node<T> {
+
+    private T data;
+    private Node<T> next;
+
+    /**
+     * parametrised constructor
+     * @param data data value held by the node
+     * @param next reference to the next node
+     */
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    /**
+     *
+     * @return data of the current node
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     *
+     * @param data value to be assigned
+     *             to the current node
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     *
+     * @return link to the next node
+     */
+    public Node<T> getNext() {
+        return next;
+    }
+
+    /**
+     *
+     * @param next link to which the current
+     *             node shall point
+     */
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    /**
+     *
+     * @return string representation of the node
+     */
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
+}
---
+++ src/_3_LinkedList/singlyLinkedList.java
@@ -0,0 +1,141 @@
+package _3_LinkedList;
+
+public class singlyLinkedList<T> {
+
+    private Node<T> head, tail;
+    private int size;
+
+    /**
+     * default constructor
+     */
+    public singlyLinkedList(){
+        head = tail = null;
+        size = 0;
+    }
+
+    /**
+     *
+     * @return true if linked list is empty
+     * false otherwise
+     */
+    public boolean isEmpty(){
+        /*If the head of the linked list is null then the list is empty*/
+        return head == null;
+    }
+
+    /**
+     *
+     * @return number of elements in the linked list
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * method to add a node at the end of the linked list
+     * @param value data value of the node to be appended
+     *              to the linked list
+     */
+    public void append(T value){
+
+        // firstly, we create the node which is to be appended to the linked list
+        Node<T> toBeAdded = new Node<>(value, null);
+
+        // Scenario 1: If the linked list is empty
+        if (isEmpty()){
+
+            /* An empty list means that both head and tail are null,
+             * accessing their next will result in null pointer exception
+             * Thus, The resultant list should be just the toBeAdded
+             */
+            head = tail = toBeAdded;
+        }
+        else {
+
+            // Scenario 2: If the linked list is non empty
+
+            /* Let the non-empty linked list be [Node (15) -> Node (16)]
+             we wish to append (10). Before appending the element
+             head = Node (15), tail = Node (16) and tail.next = null
+
+             Our resultant linked list will look like [Node (15) -> Node (16) -> Node (10)]
+             where in head = Node (15) and tail = Node (10) and tail.next = null
+             */
+
+            /* we set (tail.next = toBeAdded) so tail.next goes from (null) to (toBeAdded)
+            In other words Node (16), which is our current tail is now pointing towards Node (10)
+            */
+
+            tail.setNext(toBeAdded);
+
+            /* Now having linked Node (16) to Node (10)
+               we set tail = toBeAdded so tail goes from Node(16) to Node(10)
+             */
+            tail = toBeAdded; // current tail becomes Node (10)
+
+            // tail's next node is set to null
+            tail.setNext(null);
+        }
+
+        // increment the size of the linked list
+        size++;
+    }
+
+    /**
+     * method to check if the specified node is present in the linked list
+     * @param data value to be searched within the linked list
+     * @return node whose data value matches the given value
+     */
+    public Node<T> search (T data){
+
+        // Scenario 1: The linked list is empty
+        if (isEmpty()){
+            return null;
+        }
+
+        // Scenario 2: The linked list is non empty
+
+        // assign node as the iterator
+        Node<T> temp = head; // initialization
+
+        // iterate through the linked list
+        while (temp.getNext()!=null){
+
+            if (temp.getData() == data){
+                return temp; // node found
+            }
+
+            temp = temp.getNext(); // update statement
+        }
+
+        // After we exit the while loop, temp = last element of the linked list
+        return (temp.getData() == data ? temp : null);
+    }
+
+
+
+    /*  Function to display elements  */
+    public void display() {
+
+        System.out.print("\nSingly Linked List = ");
+
+        if (size == 0) {
+            System.out.print("empty\n");
+            return;
+        }
+
+        if (head.getNext() == null) {
+            System.out.println(head.getData() );
+            return;
+        }
+
+        Node ptr = head;
+        System.out.print(head.getData()+ " -> ");
+        ptr = head.getNext();
+        while (ptr.getNext() != null) {
+            System.out.print(ptr.getData()+ " -> ");
+            ptr = ptr.getNext();
+        }
+        System.out.print(ptr.getData()+ "\n");
+    }
+}
