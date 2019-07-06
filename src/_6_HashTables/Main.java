5de4d439a76651b24de7cfb18ff1da48aac95738
u6146791 <u6146791@anu.edu.au>
2019-07-06 17:18:04 +1000

    added main program for running simple hash table

---
+++ src/_6_HashTables/Main.java
@@ -0,0 +1,23 @@
+package _6_HashTables;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee [] employees = {
+                new Employee("Jane", "Jones", 123),
+                new Employee("John", "Doe", 4567),
+                new Employee("Mary", "Smith", 22),
+                new Employee("Mike", "Wilson", 3245),
+                new Employee("Bill", "End", 78)
+        };
+
+        SimpleHashTable hashTable = new SimpleHashTable(10);
+
+        for (Employee e: employees) {
+            hashTable.put(e.getLastName(), e);
+        }
+
+        hashTable.printHashTable();
+    }
+}
