6ccfcbf3877eb9093978536412f681b82d5eb763
u6146791 <u6146791@anu.edu.au>
2019-07-06 11:14:00 +1000

    Added queue main program

---
+++ src/_5_Queues/QueueUse.java
@@ -0,0 +1,70 @@
+package _5_Queues;
+
+import java.util.Scanner;
+
+public class QueueUse {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Creating object of class Queue */
+        QueueUsingArray<Integer> queueArray = new QueueUsingArray<>(Integer.class);
+
+        /* Perform Queue Operations */
+        System.out.println("Queue Test\n");
+        char ch;
+
+        do {
+            System.out.println("\n Queue Operations");
+            System.out.println("1. enqueue");
+            System.out.println("2. dequeue");
+            System.out.println("3. peek");
+            System.out.println("4. check empty");
+            System.out.println("5. size");
+
+            int choice = scan.nextInt();
+
+            switch (choice) {
+                case 1 :
+                    System.out.println("Enter element to be enqueued");
+                    queueArray.enqueue(scan.nextInt());
+                    break;
+
+                case 2 :
+                    try {
+                        System.out.println("dequeued Element = "+ queueArray.dequeue());
+                    }
+                    catch (Exception e) {
+                        System.out.println("Error : " + e.getMessage());
+                    }
+                    break;
+
+                case 3 :
+                    try {
+                        System.out.println("Peek Element = "+ queueArray.front());
+                    }
+                    catch (Exception e) {
+                        System.out.println("Error : " + e.getMessage());
+                    }
+                    break;
+
+                case 4 :
+                    System.out.println("Empty status = "+ queueArray.isEmpty());
+                    break;
+
+                case 5 :
+                    System.out.println("Size = "+ queueArray.size());
+                    break;
+
+                default :
+                    System.out.println("Wrong Entry \n ");
+                    break;
+            }
+
+            System.out.println("\nDo you want to continue (Type y or n) \n");
+            ch = scan.next().charAt(0);
+
+        } while (ch == 'Y'|| ch == 'y');
+    }
+}
