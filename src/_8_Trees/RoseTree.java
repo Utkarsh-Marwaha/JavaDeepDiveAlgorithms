aa9a25b00da54d3979cc1965b04b33c309f48bdc
u6146791 <u6146791@anu.edu.au>
2019-07-09 12:41:35 +1000

    Rose Tree added

---
+++ src/_8_Trees/RoseTree.java
@@ -0,0 +1,195 @@
+package _8_Trees;
+
+
+public class RoseTree<T> {
+
+    private RoseTreeNode<T> root;
+
+    /*constructor*/
+    public RoseTree(){
+        root = null;
+    }
+
+    /**
+     * method to check if the tree is empty
+     * @return true if the tree is empty
+     * false otherwise
+     */
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    /**
+     * time complexity  = O(number of nodes)
+     * space complexity = O(height of the tree)
+     *
+     * @param node root of the rose tree
+     * @return number of nodes present in the rose tree with
+     * the given root
+     */
+    public int count(RoseTreeNode<T> node){
+
+        /*if the tree is empty then there are 0 nodes present in it*/
+        if (node==null){
+            return 0;
+        }
+
+        // This variable will keep count of the number of nodes present in the tree
+        int size = 0;
+
+        // cycle through all the children of the current node
+        for (int i = 0; i < node.getChildren().size(); i++){
+
+            // recall the count() for each subtree and accumulate the result in the `size` variable
+            size += count(node.getChildren().get(i));
+        }
+
+        // add one to the size to account for the root node
+        return size+1;
+    }
+
+
+    /**
+     * time complexity  = O(number of nodes)
+     * space complexity = O(height of the tree)
+     *
+     * @param node root of the rose tree
+     * @return sum of all the nodes present in the rose tree with the given root
+     */
+    public int sum(RoseTreeNode<Integer> node){
+
+        /*if the tree is empty then its sum is 0*/
+        if (node == null){
+            return 0;
+        }
+
+        // This variable will accumulate the sum of all nodes present in the tree
+        int total = 0;
+
+        // cycle through all the children of the current node
+        for (int i = 0; i < node.getChildren().size(); i++){
+
+            // recall the sum() for each subtree and accumulate the result in the `total` variable
+            total += sum(node.getChildren().get(i));
+        }
+
+        // add root's data to the `total` at the end
+        return node.getData() + total;
+    }
+
+
+    /**
+     * time complexity  = O(number of nodes)
+     * space complexity = O(height of the tree)
+     *
+     * @param node root of the rose tree
+     * @return node with the largest value present in the rose tree
+     */
+    public int largest(RoseTreeNode<Integer> node){
+
+        /*if the tree is empty then we simply return 0*/
+        if (node == null){
+            return 0;
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        /*compare root data with data of all children's data*/
+        for (int i = 0; i < node.getChildren().size(); i++){
+
+            RoseTreeNode<Integer> child = node.getChildren().get(i);
+
+            if (max < child.getData()){
+                max = child.getData();
+            }
+        }
+
+        /*Every subtree calculates the maximum in its own hierarchy*/
+        for (int i = 0; i < node.getChildren().size(); i++){
+            int maxOfSubtree = largest(node.getChildren().get(i));
+
+            if (max < maxOfSubtree){
+                max = maxOfSubtree;
+            }
+        }
+
+        /*Check if the root is the largest element of the (sub)tree*/
+        if (max < node.getData()){
+            max = node.getData();
+        }
+
+        // return the maximum value a.k.a largest element
+        return max;
+    }
+
+    /**
+     * time complexity  = O(number of nodes)
+     * space complexity = O(height of the tree)
+     *
+     * @param node root of the rose tree
+     * @return node with the smallest value present in the rose tree
+     */
+    public int smallest(RoseTreeNode<Integer> node){
+
+        /*if the tree is empty then we simply return 0*/
+        if (node == null){
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        /*compare root data with data of all children's data*/
+        for (int i = 0; i < node.getChildren().size(); i++){
+
+            RoseTreeNode<Integer> child = node.getChildren().get(i);
+
+            if (min > child.getData()){
+                min = child.getData();
+            }
+        }
+
+        /*Every subtree calculates the minimum in its own hierarchy*/
+        for (int i = 0; i < node.getChildren().size(); i++){
+            int minOfSubtree = largest(node.getChildren().get(i));
+
+            if (min > minOfSubtree){
+                min = minOfSubtree;
+            }
+        }
+
+        /*Check if the root is the smallest element of the (sub)tree*/
+        if (min > node.getData()){
+            min = node.getData();
+        }
+
+        // return the minimum value a.k.a smallest element
+        return min;
+    }
+
+    /**
+     * time complexity  = O(number of nodes)
+     * space complexity = O(height of the tree)
+     *
+     * @param node root of the rose tree
+     * @return height of the rose tree
+     */
+    public int height(RoseTreeNode<T> node){
+
+        if (node == null){
+            return 0;
+        }
+
+        int maxHeight = 0;
+
+        for (int i = 0; i < node.getChildren().size(); i++){
+
+            int heightOfSubtree = height(node.getChildren().get(i));
+
+            if (maxHeight > heightOfSubtree){
+                maxHeight = heightOfSubtree;
+            }
+        }
+
+        return maxHeight+1;
+    }
+}
---
+++ src/_8_Trees/RoseTreeNode.java
@@ -0,0 +1,55 @@
+package _8_Trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoseTreeNode<T>{
+
+
+    private T data;
+    private List<RoseTreeNode<T>> children;
+
+    /* default constructor */
+    public RoseTreeNode(){
+        data = null;
+        children = null;
+    }
+
+    /* parametrised constructor */
+    public RoseTreeNode(T data, List<RoseTreeNode<T>> children) {
+        this.data = data;
+        this.children = children;
+    }
+
+    /**
+     * method to set(modify) the data of the current tree node
+     * @return data value of the tree node
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * method to get(access) the data of the current tree node
+     * @param data value of the tree node
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     * method to get(access) to the children of a parent node
+     * @return list of children nodes of the current node
+     */
+    public List<RoseTreeNode<T>> getChildren() {
+        return children;
+    }
+
+    /**
+     * method to set(modify) the data of the children of a parent node
+     * @param children list of children of the current node
+     */
+    public void setChildren(ArrayList<RoseTreeNode<T>> children) {
+        this.children = children;
+    }
+}
